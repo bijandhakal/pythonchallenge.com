3dc500328c65b59bc40e9084a4f7250763c40805
bijandakal <dhakal.bijan@gmail.com>
2015-10-30 11:37:51 +0545

    using non greedy regex and drawing output in new image

--- 09.py
+++ 09.py
@@ -8,12 +8,13 @@ URL = 'http://www.pythonchallenge.com/pc/return/good.html'
 IMAGE_URL = 'http://www.pythonchallenge.com/pc/return/good.jpg'
 
 page = Authentication(URL,'huge','file').access()
-first = ''.join(findall(r'first:(.*)\n\nsecond', page, DOTALL)).split(',')
+first = ''.join(findall(r'first:(.*?)\n\n', page, DOTALL)).split(',')
 first = map(lambda x: int(x), first)
-second = ''.join(findall(r'second:(.*)\n\n', page, DOTALL)).split(',')
+second = ''.join(findall(r'second:(.*?)\n\n', page, DOTALL)).split(',')
 second = map(lambda x: int(x), second)
-image = Image.open(StringIO(Authentication(URL,'huge','file').access()))
-draw = ImageDraw.Draw(image)
+image = Image.open(StringIO(Authentication(IMAGE_URL,'huge','file').access()))
+new_image = Image.new(image.mode,image.size,color='#FFFFFF')
+draw = ImageDraw.Draw(new_image)
 draw.line(first,'red')
 draw.line(second,'red')
-image.show()
+new_image.show()
